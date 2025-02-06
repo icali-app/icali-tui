a8abb430a90c90d52cf8ea944193703c285e1753
Giftzwerg02 <chessplayer1@gmx.at>
2025-02-06 22:32:45 +0100

    basic app

--- cmd/icali-tui/main.go
+++ cmd/icali-tui/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/icali-app/icali-tui/internal/components/app"
 )
 
 func main() {
-
+	app := app.Model{}
+	if _, err := tea.NewProgram(app).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
 }
---
+++ internal/components/app/app.go
@@ -0,0 +1,19 @@
+package app
+
+import tea "github.com/charmbracelet/bubbletea"
+
+type Model struct {
+
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m Model) View() string {
+	return ""
+}
